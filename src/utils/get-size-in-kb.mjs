82ac11b9f2ce8a6ccb2ccdd8cc619e386f7f165b
Jonathan Perry <jonathanperry@sequencemedia.net>
2023-06-14 19:03:20 +0100

    Byte length

--- src/utils/get-size-in-kb.mjs
+++ src/utils/get-size-in-kb.mjs
@@ -1,31 +1,7 @@
-/*
-export default function getSizeInKB (string) {
-  let i = string.length - 1
-  let j = string.length
-
-  for (i; i >= 0; i--) {
-    const code = string.charCodeAt(i)
-    if (code > 0x7f && code <= 0x7ff) {
-      j += 1
-    } else {
-      if (code > 0x7ff && code <= 0xffff) {
-        j += 2
-      }
-    }
-
-    if (code >= 0xDC00 && code <= 0xDFFF) {
-      i-- // trail surrogate
-    }
-  }
-
-  return (j || 0) / 1000
-}
-*/
-
-export default function getSizeInKB (string) {
+export default function getSizeInKB (data) {
   const {
     byteLength
-  } = Buffer.from(string)
+  } = Buffer.from(data)
 
   return (
     byteLength / 1000
